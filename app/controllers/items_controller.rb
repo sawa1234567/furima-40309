442c071da57c8d217b4cd8be630a08624c96a7ad
sawa1234567 <ss.sawa80@gmail.com>
2024-05-07 18:24:45 +0900

    Revert "ユーザー管理機能実装"
    This reverts commit fcb4f0b6eadee1193a76a1c53568894fa11ac530.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,4 @@
 class ItemsController < ApplicationController
-
   def index
-    #@items = Item.all
   end
-
-  # def new
-  #   @item = Item.new
-  # end
-
-  # def create
-  #   Item.create(item_params)
-  #   redirect_to '/'
-  # end
-
-  # private
-  # def item_params
-  #   #params.require(:item).permit(:name)
-  # end
 end

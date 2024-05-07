9ec6a741d68483e764b5ccea2f99da3ae4fec0ce
sawa1234567 <ss.sawa80@gmail.com>
2024-05-08 01:32:14 +0900

    手数料計算

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
     validates :item_status_id
     validates :shipping_cost_id
     validates :prefecture_id
-    validates :date_id
+    validates :shipping_date_id
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 end

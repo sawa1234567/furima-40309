25edd59031b9ca44e98d26ca241eec3a9dae51cf
sawa1234567 <ss.sawa80@gmail.com>
2024-05-07 21:58:20 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,6 +15,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:image, :item_name, :item_explain, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :date, :price)
+    params.require(:item).permit(:image, :item_name, :item_explain, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :Shipping_date_id, :price)
   end
 end

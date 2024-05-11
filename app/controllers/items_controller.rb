7b0385ed6d91c51deb10ef06f0a9d21c4c3ae59d
sawa1234567 <ss.sawa80@gmail.com>
2024-05-11 21:28:08 +0900

    商品詳細機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :set_item, only:[:show]
   def index
     @items = Item.includes(:user).order(created_at: :desc)
   end
@@ -20,8 +20,15 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :item_name, :item_explain, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id:current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
